c4071b7320c2e9bb24c2ba374b8b0d088520f45d
karshann <karshan951@gmail.com>
2020-03-26 16:47:23 +0100

    ajout checkstyle.xml + modif pom + creation main

---
+++ src/main/java/org/example/Applicationmain.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public enum Applicationmain {
+    APPLICATION;
+
+    public void run(String [] args){
+
+    }
+    public static void main(String [] args)
+    {
+        APPLICATION.run(args);
+    }
+}
